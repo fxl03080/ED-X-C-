4738e3d77d9b5be7c85d6b33b42c3a740764e77a
Frank Landry <FLandry@vtc.vsc.edu>
2017-08-04 18:08:09 -0400

    Update Assignments.cpp

--- Assignments.cpp
+++ Assignments.cpp
@@ -48,3 +48,39 @@ Use an appropriate name */
 
 enum monthsOfYear { January = 1, February, March, April, May, June, July, August, September, October, November, December}; 
 
+
+/* The scenario is that you have been asked to create a library of utility functions that support mathematical functions. 
+You will add this code to functions in the next module but for now, write code to complete the following:
+
+Using a for loop, write code that will compute the result of an int raised to the power of another int. 
+For example, your for loop should use two variables, one for the base and one for the exponent. 
+It should then calculate the base raised to the exponent. 
+2 raised to the power of 2 should output 4, 2 raised to 8 should output 256, etc. 
+Ensure your code meets these requirements and then paste it in the response section below:
+
+Contains a variable for the base
+Contains a variable for the exponent
+Uses a for loop to perform the power function
+Outputs the result to the console window */
+int w = 8;
+for (int i {0}; i < w; i ++) {
+ cout << power(i, w);
+}
+
+
+/* For this second response, you will create a switch statement. The switch statement should include the following:
+
+A variable of type char for the comparison
+The switch statement should check for the value of 'y' (lowercase) or 'Y' (uppercase) and output the response 
+"You chose y or Y" to the console window
+The switch statement should check for the value 'n' or 'N' and output the response, 
+"You chose n or N" to the console window
+The switch statement should include the correct component that will output 
+"You didn't choose a valid option" if neither of these two previous conditions are true */ 
+char equalSign = '==';
+switch(1) {
+  case 'y'+equalSign+'Y': cout << 'You chose y or Y'; break;
+  case 'n'+equalSign+'N': cout << 'You chose n or N'; break;
+  default: cout << 'You didn\'t choose a valid option'; break;
+}
+
